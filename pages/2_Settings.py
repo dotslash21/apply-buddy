a5837c3f6a9ef41c8bb27a8c179e8c4ac710103e
Arunangshu Biswas <arunangshubsws@gmail.com>
2023-08-04 22:16:02 +0530

    fix: updated required msg for open api key input

--- pages/2_Settings.py
+++ pages/2_Settings.py
@@ -10,7 +10,7 @@ st.subheader('Settings')
 
 # Get API keys
 openai_api_key = st.text_input("OpenAI API Key", value=st.session_state.openai_api_key, type="password")
-st.caption("*Required for all apps; get it [here](https://platform.openai.com/account/api-keys).*")
+st.caption("*Required for all services; get it [here](https://platform.openai.com/account/api-keys).*")
 
 # If the 'Save' button is clicked
 if st.button("Save"):
